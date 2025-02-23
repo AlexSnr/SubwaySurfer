f8a944cbaadea485f92ec699e07c96d0cf70b01e
Alex <aschander062@gmail.com>
2025-02-23 14:12:54 +0100

    Weiterentwicklung Gleise und Mensch 3 Gleisen werden per Schleife erzeugt Mensch springt nach Links Mensch läuft Geradeaus

--- Mensch.java
+++ Mensch.java
@@ -1,13 +1,14 @@
 import GLOOP.*;
 public class Mensch{
+    GLQuader meinTorso;
+    GLKugel meinKopf;
+    GLZylinder meinArmR;
+    GLZylinder meinArmL;
+    GLZylinder meinBeinR;
+    GLZylinder meinBeinL;
     public Mensch(int posX, int posY, int posZ){
     
-        GLQuader meinTorso;
-        GLKugel meinKopf;
-        GLZylinder meinArmR;
-        GLZylinder meinArmL;
-        GLZylinder meinBeinR;
-        GLZylinder meinBeinL;
+        
         
         meinTorso = new GLQuader(posX+0,posY+20,posZ+0,20,30,10);
         meinKopf = new GLKugel(posX+0,posY+45,posZ+0,10);
@@ -27,21 +28,45 @@ public class Mensch{
         meinArmL.setzeFarbe(1,0.5,0.25);
         meinBeinR.setzeFarbe(0.1,0.1,0.9);
         meinBeinL.setzeFarbe(0.1,0.1,0.9);
-    
-    
-    
-    
-    
-    
-    
+        
+        
     
     
     }
 
+    public void bewege(){
+    
+        meinKopf.verschiebe(0, 0, -1);
+        meinTorso.verschiebe(0, 0, -1);
+        meinArmR.verschiebe(0, 0, -1);
+        meinArmL.verschiebe(0, 0, -1);
+        meinBeinR.verschiebe(0, 0, -1);
+        meinBeinL.verschiebe(0, 0, -1);
 
+    }
+    
+    public void springelinks(){
+        for(int i = 0;i<220;i++){
+            meinKopf.verschiebe(-0.5, 0, 0);
+            meinTorso.verschiebe(-0.5, 0, 0);
+            meinArmR.verschiebe(-0.5, 0, 0);
+            meinArmL.verschiebe(-0.5, 0, 0);
+            meinBeinR.verschiebe(-0.5, 0, 0);
+            meinBeinL.verschiebe(-0.5, 0, 0);
+        }
+    }
+    
+    public void springerechts(){
+        for(int i = 0;i<220;i++){
+            meinKopf.verschiebe(0.5, 0, 0);
+            meinTorso.verschiebe(0.5, 0, 0);
+            meinArmR.verschiebe(0.5, 0, 0);
+            meinArmL.verschiebe(0.5, 0, 0);
+            meinBeinR.verschiebe(0.5, 0, 0);
+            meinBeinL.verschiebe(0.5, 0, 0);
+        }
 
-
-
+    }
 
 
 }
--- Spielverwalter.java
+++ Spielverwalter.java
@@ -10,13 +10,12 @@ public class Spielverwalter{
         meinlicht = new GLLicht();
        
         Zug meinZug;
-        meinZug = new Zug(200,64,0);
+        meinZug = new Zug(110,70,0);
         
-        Gleis meinGleis;
-        meinGleis = new Gleis(0,55,0);
+        
         
         Mensch meinMensch;
-        meinMensch = new Mensch(200,30,200);
+        meinMensch = new Mensch(0,30,200);
         
         GLBoden meinBoden;
         meinBoden = new GLBoden("Boden.jpg");
@@ -24,16 +23,47 @@ public class Spielverwalter{
         GLHimmel meinHimmel;
         meinHimmel = new GLHimmel("Himmel.jpg");
         
-    
+        GLTastatur meineTastatur;
+        meineTastatur = new GLTastatur();
         
         
-       
-    
+        gleisenaufbau();
+
+        boolean tastelosgelassen = true;
+        while(meineTastatur.istGedrueckt('x')==false){
+            
+            if(meineTastatur.istGedrueckt('w')){
+        
+                meinMensch.bewege();
+                meinekamera.verschiebe(0,0,-1);
+
+            }
+            if(tastelosgelassen){
+                if(meineTastatur.istGedrueckt('a')){
+            
+                    meinMensch.springelinks();
+                    tastelosgelassen = false;
+                }
+            }
+            if(!meineTastatur.istGedrueckt('a')){
+                tastelosgelassen = true;
+            }
+            Sys.warte();
+        }
+
     }
                     
-    
-    
-    
-    
-    
+    public void gleisenaufbau(){
+        for(int i = 0;i<100;i++){
+            Gleis meinGleis;
+            meinGleis = new Gleis(0,55,0+i*(-300));
+            Gleis meinGleisL;
+            meinGleis = new Gleis(-110,55,0+i*(-300));
+            Gleis meinGleisR;
+            meinGleis = new Gleis(110,55,0+i*(-300));
+   
+        }
+
+    }
+   
 }
